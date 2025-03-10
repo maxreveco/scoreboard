43d3b25d177e397190719e9f20c6636eac87b668
Max Reveco <max.revecog@hotmail.com>
2025-03-10 17:59:11 -0300

    Refactor functions to use arguments instead of variables inside

--- index.js
+++ index.js
@@ -10,35 +10,15 @@ let guestPointCounter = 0
 
 // Home point buttons functions
 
-function add1PointsHome() {
-    homePointCounter += 1
-    homeScore.textContent = homePointCounter
-}
-
-function add2PointsHome() {
-    homePointCounter += 2
-    homeScore.textContent = homePointCounter
-}
-
-function add3PointsHome() {
-    homePointCounter += 3
+function addPointsHome(point) {
+    homePointCounter += point
     homeScore.textContent = homePointCounter
 }
 
 // Guest point buttons functions
 
-function add1PointsGuest() {
-    guestPointCounter += 1
-    guestScore.textContent = guestPointCounter
-}
-
-function add2PointsGuest() {
-    guestPointCounter += 2
-    guestScore.textContent = guestPointCounter
-}
-
-function add3PointsGuest() {
-    guestPointCounter += 3
+function addPointsGuest(point) {
+    guestPointCounter += point
     guestScore.textContent = guestPointCounter
 }
 
@@ -47,8 +27,8 @@ function add3PointsGuest() {
 function resetScores() {
     homePointCounter = 0
     guestPointCounter = 0
-    
+
     homeScore.textContent = homePointCounter
     guestScore.textContent = homePointCounter
-    
+
 }
